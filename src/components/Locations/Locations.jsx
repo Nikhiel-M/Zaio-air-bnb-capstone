2ed55b3ec98ae7b57829a4d7cf0f65fb74358b4a
Nikhiel-M <164867669+Nikhiel-M@users.noreply.github.com>
2026-02-04 11:14:33 +0200

    Fixed link bug in api.js

--- src/components/Locations/Locations.jsx
+++ src/components/Locations/Locations.jsx
@@ -36,7 +36,7 @@ const Locations = () => {
 
   // const ENDPOINT = `${getApiBase() || 'http://localhost:5000'}/api/properties`;
 
-   const ENDPOINT = `https://zaio-air-bnb-capstone.onrender.com/api/locations/properties`;
+   const ENDPOINT = `https://zaio-air-bnb-capstone.onrender.com/api/properties`;
   //  http://localhost:5000/api/properties
 
   useEffect(() => {
