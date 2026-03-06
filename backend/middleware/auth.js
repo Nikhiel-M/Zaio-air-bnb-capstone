902fd3c01c3a443345cb2c535a8f365119f6a724
Nikhiel-M <164867669+Nikhiel-M@users.noreply.github.com>
2026-03-06 14:11:09 +0200

    updated auth

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -12,9 +12,9 @@ const auth = async (req, res, next) => {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     
     const user = await User.findById(decoded.userId); 
-    if (!user) {
-      return res.status(401).json({ message: 'Token is not valid' });
-    }
+    // if (!user) {
+    //   return res.status(401).json({ message: 'Token is not valid' });
+    // }
 
     req.userId = user._id;
     req.user = user;
