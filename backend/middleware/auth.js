cd810e5ff76f875778cd632e1534bc164cafb739
Nikhiel-M <164867669+Nikhiel-M@users.noreply.github.com>
2026-03-09 00:48:41 +0200

    auth refactored

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,43 +1,23 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
-import { authAPI } from "../../src/services/api";
+import jwt from 'jsonwebtoken';
+import User from '../models/User.js';
 
 const auth = async (req, res, next) => {
-  const logout = () => {
-    authAPI.logout();
-    console.log("Token is not valid, user logged out.")
-  }
-
   try {
     const token = req.header('Authorization')?.replace('Bearer ', '');
-    
     if (!token) {
-      logout();
-      return res.status(402).json({
-        message: "No token or expired token"
-      })
-
+      return res.status(401).json({ message: 'No token or expired token' });
     }
-
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    
-    const user = await User.findById(decoded.userId); 
-    // if (!user) {
-    //   return res.status(401).json({ message: 'Token is not valid' });
-    // }
-
-
+    const user = await User.findById(decoded.userId);
+    if (!user) {
+      return res.status(401).json({ message: 'Token is not valid' });
+    }
     req.userId = user._id;
     req.user = user;
-    
     next();
   } catch (error) {
-
-    // console.error('Auth middleware error:', error);
-    // res.status(401).json({ message: 'Token is not valid' });
-
+    return res.status(401).json({ message: 'Token is not valid' });
   }
-
 };
 
-module.exports = auth;
+export default auth;
