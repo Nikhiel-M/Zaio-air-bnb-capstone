0a9b1b715956797df452aebc9ed55aea0d7b6c1f
Nikhiel-M <164867669+Nikhiel-M@users.noreply.github.com>
2026-03-14 11:20:48 +0200

    debugged updateProperty

--- backend/routes/properties.js
+++ backend/routes/properties.js
@@ -15,7 +15,7 @@ const router = express.Router();
 router.get("/", getAllProperties);
 router.get("/:id", getPropertyById);
 router.post("/", auth, upload.array("images", 5), createProperty);
-router.put("/:id", auth, upload.array("images", 5), updateProperty);
+router.patch("/:id", auth, upload.array("images", 5), updateProperty);
 router.delete("/:id", auth, deleteProperty);
 router.get("/search/location", searchPropertiesByLocation);
 
