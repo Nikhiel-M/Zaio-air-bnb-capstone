b9347d245b0c680fcbc730b58539bb9277f4591b
Nikhiel-M <164867669+Nikhiel-M@users.noreply.github.com>
2026-04-20 15:20:10 +0200

    updated host page

--- src/Pages/HostPage/HostPage.jsx
+++ src/Pages/HostPage/HostPage.jsx
@@ -1,10 +1,21 @@
 import { useHostGuard } from "../../services/hooks";
+import { PillButton } from '../../components/Buttons/PillButton.styled';
+import {HostPageContainer, HostPageHeader, HostPageBody } from "./HostPage.styled";
 
 const HostPage = () => {
   useHostGuard();
   
   return (
-    <div>HostPage</div>
+    <HostPageContainer>
+      <HostPageHeader>
+        <div className="pill-container">
+        <PillButton className="btn">List a new property</PillButton>
+        <PillButton className="btn">List a new property</PillButton>
+        <PillButton className="btn">List a new property</PillButton>
+        </div>
+      </HostPageHeader>
+      <HostPageBody>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor debitis itaque pariatur reprehenderit perferendis voluptate sed sequi quisquam hic illo. Soluta eius, optio expedita ratione atque quae suscipit nesciunt necessitatibus.</HostPageBody>
+    </HostPageContainer>
   );
 }
 
--- src/Pages/HostPage/HostPage.styled.js
+++ src/Pages/HostPage/HostPage.styled.js
@@ -1,1 +1,30 @@
 import styled from "styled-components";
+import { Container } from "../../Styles/General.styled";
+
+export const HostPageContainer = styled(Container)`
+display: flex;
+flex-direction: column;
+
+`
+
+export const HostPageHeader = styled.div`
+display: flex;
+align-self: start;
+
+
+.pill-container{
+    display: flex;
+    margin: 1.5rem 0 5rem 0;
+    gap: 3rem;
+    flex-direction: row;
+    align-items: flex-start;
+
+}
+
+.btn {
+    font-size: 0.9rem;
+    font-weight: 300;
+}
+`
+
+export const HostPageBody = styled.div``
