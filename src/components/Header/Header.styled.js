de21f21448cf0871e92318af3393ccf1c45544a3
Nikhiel-M <164867669+Nikhiel-M@users.noreply.github.com>
2026-04-20 13:07:25 +0200

    updated header

--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const HeaderContainer = styled.div`
   padding: 1rem 0 1rem 0;
   width: 100%;
-  height: 15%;
+  height: auto;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
