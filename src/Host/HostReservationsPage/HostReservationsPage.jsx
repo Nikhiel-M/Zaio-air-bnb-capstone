c74b99f3bf4254b1a35850d375e0adcf172659c1
Nikhiel-M <164867669+Nikhiel-M@users.noreply.github.com>
2026-05-21 13:14:32 +0200

    Changed the name of ReservationPage to HostReservationPage

--- src/Host/HostReservationsPage/HostReservationsPage.jsx
+++ src/Host/HostReservationsPage/HostReservationsPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ReservationsHeader, ReservationsListContainer, ReservationsContainer } from './ReservationsPage.styled'
+import { ReservationsHeader, ReservationsListContainer, ReservationsContainer } from './HostReservationsPage.styled'
 import GridComponent from '../../components/GeneralComponents/GridComponent/GridComponent'
 import { useHostGuard } from '../../services/hooks'
 import HostHeaderComponent from '../HostComponents/HostHeaderComponent'
